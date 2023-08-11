10f84503e689c65912963b6604f64a41e4feab88
Aditya Khedekar <adityakhedekar98906@gmail.com>
2023-08-11 17:59:03 +0530

    added local storage functionality

--- js/favorites.js
+++ js/favorites.js
@@ -1,31 +1,68 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const favoritesList = document.getElementById("favoritesList");
-    const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
-  
-    function updateFavoritesList() {
-      favoritesList.innerHTML = "";
-  
-      for (const superhero of favorites) {
-        const listItem = document.createElement("li");
-        listItem.textContent = superhero.heroname;
-  
-        const removeButton = document.createElement("button");
-        removeButton.textContent = "Remove from Favorites";
-        removeButton.addEventListener("click", () => {
-          removeFromFavorites(superhero.heroname);
-        });
-  
-        listItem.appendChild(removeButton);
-        favoritesList.appendChild(listItem);
-      }
-    }
-  
-    function removeFromFavorites(heroname) {
-      const updatedFavorites = favorites.filter(character => character.heroname !== heroname);
-      localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
-      updateFavoritesList();
-    }
-  
-    updateFavoritesList();
-  });
-  
+// function displayFavorites() {
+//   // Get favorites from localStorage
+//   let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+
+//   // Clear favorites container
+//   const favoritesContainer = document.querySelector("#favorites-container");
+//   favoritesContainer.innerHTML = "";
+
+//   // Display favorite superheroes
+//   favorites.forEach(id => {
+//     // Create superhero element
+//     const superheroElement = document.createElement("div");
+//     superheroElement.classList.add("superhero");
+
+//     // Get superhero data from API
+//     const apiUrl = `http://gateway.marvel.com/v1/public/characters/${id}?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
+//     fetchData(apiUrl).then(data => {
+//       const character = data[0];
+//       const { thumbnail, name } = character;
+//       const imageExtension = thumbnail.extension;
+//       const fullImagePath = `${thumbnail.path}.${imageExtension}`;
+
+//       // Create image element
+//       const imgElement = document.createElement("img");
+//       imgElement.src = fullImagePath;
+//       imgElement.classList.add("superhero-image");
+
+//       // Create name element
+//       const nameElement = document.createElement("p");
+//       nameElement.classList.add("superhero-name");
+//       nameElement.textContent = name;
+
+//       // Create remove button
+//       const removeButton = document.createElement("button");
+//       removeButton.classList.add("remove-button");
+//       removeButton.textContent = "Remove from favorites";
+//       removeButton.onclick = () => {
+//         removefromfavorites(id);
+//         displayFavorites();
+//       };
+
+//       // Append elements to superhero element
+//       superheroElement.appendChild(imgElement);
+//       superheroElement.appendChild(nameElement);
+//       superheroElement.appendChild(removeButton);
+//     });
+
+//     // Append superhero element to favorites container
+//     favoritesContainer.appendChild(superheroElement);
+//   });
+// }
+
+// window.onload = () => {
+//   displayFavorites();
+// };
+
+
+// Retrieve the data from localStorage
+let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+
+favorites.forEach(hero=>{
+  console.log(`Name: ${hero.heroname}`);
+  console.log(`Image: ${hero.imageSrc}`);
+  console.log(`Comics: ${hero.comics}`);
+  console.log(`Stories: ${hero.stories}`);
+  console.log(`Series: ${hero.series}`);
+  console.log(`Description: ${hero.description}`);
+})
--- js/script.js
+++ js/script.js
@@ -1,28 +1,34 @@
-// import CryptoJS from 'CryptoJS';
 const publicKey = "1f0bcb9b187fc93f847f1cafd9f85468";
 const privateKey = "10b70287677eba0342d98cc0e6a85fcd5e37846c";
 const ts = Date.now();
 const hash = CryptoJS.MD5(ts + privateKey + publicKey).toString();
 const apiurl = `http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
-// console.log(apiurl);
-let apiurl1;
-let favorites  = []
-let id, series, stories, comics, description;
-const searchString = document.getElementById("searchResult");
 
-searchString.addEventListener("input", function () {
+const searchString = document.getElementById("searchResult");
+const characterImageContainer = document.getElementById("characterImageContainer");
+const dialogElement = document.querySelector("#dialog");
+const modalImageElement = document.querySelector("#modalImage");
+const modalName = document.querySelector("#modalName-span");
+const modalComics = document.querySelector("#modalComics-span");
+const modalStories = document.querySelector("#modalStories-span");
+const modalSeries = document.querySelector("#modalSeries-span");
+const modalDescription = document.querySelector("#modalDescription-span");
+const modalId = document.querySelector("#charId");
+
+searchString.addEventListener("input", async function () {
   const searchResultValue = searchString.value;
-  hitapi(searchResultValue);
+  await hitapi(searchResultValue);
 });
 
 async function hitapi(textSearch) {
   try {
-    if (textSearch) {
-      apiurl1 = `http://gateway.marvel.com/v1/public/characters?nameStartsWith=${textSearch}&ts=${ts}&apikey=${publicKey}&hash=${hash}`;
-      await fetchData(apiurl1);
-    } else {
-      await fetchData(apiurl);
-    }
+    const apiUrl = textSearch ? 
+      `http://gateway.marvel.com/v1/public/characters?nameStartsWith=${textSearch}&ts=${ts}&apikey=${publicKey}&hash=${hash}` : 
+      apiurl;
+    const data = await fetchData(apiUrl);
+    
+    clearCharacterImages();
+    displayCharacters(data);
   } catch (error) {
     console.error("Error:", error);
   }
@@ -32,160 +38,104 @@ async function fetchData(apiUrl) {
   try {
     const response = await fetch(apiUrl);
     const data = await response.json();
-    const characters = data.data.results;
-
-    clearCharacterImages();
-    displayCharacters(characters);
-
-    const characterImageContainer = document.getElementById("characterImageContainer");
-    const imageElements = characterImageContainer.querySelectorAll(".image-card");
-
-    for (let i = 0; i < imageElements.length; i++) {
-      const imageElement = imageElements[i];
-      imageElement.addEventListener("click", function () {
-        const imageSrc = this.src;
-        const heroname = this.nextElementSibling.textContent;
-        const descriptionElement = this.nextElementSibling.nextElementSibling;
-        const description = descriptionElement.textContent;
-        const id = characters[i].id;
-        displayModalView(id, imageSrc, heroname, comics, stories, series, description);
-      });
-    }
+    return data.data.results;
   } catch (error) {
-    console.error("Error:", error);
+    throw error;
   }
 }
+
 function clearCharacterImages() {
-  const characterImageContainer = document.getElementById("characterImageContainer");
   characterImageContainer.innerHTML = "";
 }
 
 function displayCharacters(characters) {
-  for (let i = 0; i < 24; i++) {
-    const characterImage = characters[i].thumbnail.path;
-    // console.log(characterImage)
-    const heroname = characters[i].name;
-    description = characters[i].description;
-    // console.log(description)
-    comics = characters[i].comics.available;
-    series = characters[i].series.available;
-    stories = characters[i].stories.available;
-    id = characters[i].id
-    console.log(id)
+  characters.forEach(character => {
+    const { thumbnail, name, description, comics, series, stories, id } = character;
+    if (!thumbnail.path.includes("image_not_available") && !thumbnail.path.includes("4c00358ec7548")) {
+      const imageExtension = thumbnail.extension;
+      const fullImagePath = `${thumbnail.path}.${imageExtension}`;
+      
+      const characterDiv = document.createElement("div");
+      characterDiv.classList.add("character-card");
 
-    const exclude = ["image_not_available", "4c00358ec7548"];
+      const imgElement = document.createElement("img");
+      imgElement.src = fullImagePath;
+      imgElement.classList.add("image-card");
+      imgElement.addEventListener("click", () => displayModalView(id, fullImagePath, name, comics.available, stories.available, series.available, description));
 
-    if (exclude.some((image) => characterImage.includes(image))) {
-      continue;
+      const nameElement = document.createElement("p");
+      nameElement.classList.add("imageName");
+      nameElement.textContent = name;
+
+      characterDiv.appendChild(imgElement);
+      characterDiv.appendChild(nameElement);
+      characterImageContainer.appendChild(characterDiv);
     }
-    const imageExtension = characters[i].thumbnail.extension;
-    const fullImagePath = characterImage + "." + imageExtension;
-
-    const characterDiv = document.createElement("div");
-    characterDiv.classList.add("character-card");
-
-    const imgElement = document.createElement("img");
-    imgElement.src = fullImagePath;
-    imgElement.classList.add("image-card");
-
-    const nameElement = document.createElement("p");
-    nameElement.classList.add("imageName");
-    nameElement.textContent = heroname;
-
-
-    const descriptionElement = document.createElement("p");
-    descriptionElement.classList.add("imageDescription");
-    descriptionElement.textContent = description;
-    descriptionElement.style.display = "none";
-
-
-    characterDiv.appendChild(imgElement);
-    characterDiv.appendChild(nameElement);
-
-    characterDiv.appendChild(descriptionElement);
-
-
-    const characterImageContainer = document.getElementById("characterImageContainer");
-    characterImageContainer.appendChild(characterDiv);
-  }
+  });
 }
 
-function displayModalView(id,imageSrc, heroname, comics, stories, series, description) {
-  const dialogElement = document.querySelector("#dialog");
-  const modalImageElement = document.querySelector("#modalImage");
-  const modalName = document.querySelector("#modalName-span");
-  const modalComics = document.querySelector("#modalComics-span");
-  const modalStories = document.querySelector("#modalStories-span");
-  const modalSeries = document.querySelector("#modalSeries-span");
-  const modalDescription = document.querySelector("#modalDescription-span");
-  const modalId = document.querySelector("#charId");
+function isFavorite(id) {
+  let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+  return favorites.some(fav => fav.id === id);
+}
 
-  const parentElement = document.getElementById("dialog");
 
-  // Check if the favourite and remove elements already exist
-  const existingFavourite = parentElement.querySelector(".favHero");
-  const existingRemove = parentElement.querySelector(".removeHero");
-  
-  // If the favourite element does not exist, create and append it
-  if (!existingFavourite) {
-    const favourite = document.createElement("button");
-    favourite.textContent = "Favourite";
-    favourite.classList.add("favHero");
-    favourite.addEventListener("click",function(){
-      addToFavorites(imageSrc,heroname,comics,stories,series,description)
-    });
-    parentElement.appendChild(favourite);
+function displayModalView(id, imageSrc, heroname, comics, stories, series, description) {
+  modalImageElement.src = imageSrc;
+  modalName.textContent = heroname;
+  modalComics.textContent = comics;
+  modalStories.textContent = stories;
+  modalSeries.textContent = series;
+  // modalId.textContent = id;
+  modalDescription.textContent = description || "No description!";
+
+  // Update favHero and removeHero buttons
+  if (isFavorite(id, imageSrc, heroname, comics, stories, series, description)) {
+    getfavHero.disabled = true;
+    getremoveHero.disabled = false;
+  } else {
+    getfavHero.disabled = false;
+    getremoveHero.disabled = true;
   }
   
-  // If the remove element does not exist, create and append it
-  if (!existingRemove) {
-    const remove = document.createElement("button");
-    remove.textContent = "Remove"
-    remove.classList.add("removeHero");
-    parentElement.appendChild(remove);
-  }
-  
-    modalImageElement.src = imageSrc;
-    modalName.textContent = heroname;
-    modalComics.textContent =  comics;
-    modalStories.textContent = stories;
-    modalSeries.textContent =  series;
-    modalId.textContent = id;
-    
-    if (description) {
-      modalDescription.textContent = description;
-    } else {
-      modalDescription.textContent = "No description!";
-    }
+  // Add event listeners to favHero and removeHero buttons
+  getfavHero.onclick = () => addtofavorites(id, imageSrc, heroname, comics, stories, series, description);
+  getremoveHero.onclick = () => removefromfavorites(id, imageSrc, heroname, comics, stories, series, description);
 
   dialogElement.showModal();
 }
 
-hitapi();
-
-
-function addToFavorites(imageSrc, heroname, comics, stories, series, description) {
-  const favoriteCharacter = {
-    imageSrc,
-    heroname,
-    comics,
-    stories,
-    series,
-    description
-  };
-  
-  favorites.push(favoriteCharacter);
-  console.log(favorites)
+function addtofavorites(id, imageSrc, heroname, comics, stories, series, description) {
+  let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+  let hero = {id, imageSrc, heroname, comics, stories, series, description};
+  let index = favorites.findIndex(fav => fav.id === id);
+  if (index === -1) {
+    favorites.push(hero);
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+    getfavHero.disabled = true;
+    getremoveHero.disabled = false;
+    console.log(`Added character with id ${id} to favorites ${imageSrc} ${heroname} ${comics} ${description}`);
+  } else {
+    console.log(`Character with id ${id} is already in favorites ${imageSrc} ${heroname} ${comics} ${description}`);
+  }
 }
 
-const getFav = document.querySelector(".favHero");
-const removeHero = document.querySelector(".removeHero");
 
-getFav.addEventListener("click", function(){
+function removefromfavorites(id) {
+  let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+  let index = favorites.findIndex(fav => fav.id === id);
+  if (index > -1) {
+    favorites.splice(index, 1);
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+    getfavHero.disabled = false;
+    getremoveHero.disabled = true;
+    console.log(`Removed character with id ${id} from favorites`);
+  } else {
+    console.log(`Character with id ${id} is not in favorites`);
+  }
+}
 
-})
+hitapi();
 
-removeHero.addEventListener("click",function(){
-  const modalNameElement = document.querySelector("#modalName-span");
-  const heroname = modalNameElement.textContent;
-})
+const getfavHero = document.querySelector(".favHero")
+const getremoveHero = document.querySelector(".removeHero")
